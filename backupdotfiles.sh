3c159533a07eece1c90ee6f6eb52e840c60d807b
--global <--global>
2021-11-11 15:49:51 -0700

    Modified 2021-11-11 15:49:51

---
+++ backupdotfiles.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+# check to see is git command line installed in this machine
+IS_GIT_AVAILABLE="$(git --version)"
+if [[ $IS_GIT_AVAILABLE == *"version"* ]]; then
+  echo "Git is Available"
+else
+  echo "Git is not installed"
+  exit 1
+fi
+
+
+# copy Vs-Code files
+cp -r  $HOME/.config/{alacritty,gtk*,i3,kitty,neofetch,oh-my-zsh,picom,polybar,rofi} $HOME/dotfiles/
+
+# copy other dot files
+cp  $HOME/{.zshrc,.xinitrc} $HOME/dotfiles
+
+
+
+# Check git status
+gs="$(git status | grep -i "modified")"
+# echo "${gs}"
+
+# If there is a new change
+if [[ $gs == *"modified"* ]]; then
+  echo "push"
+fi
+
+
+# push to Github
+git add .;
+git commit -m "Modified `date +'%Y-%m-%d %H:%M:%S'`";
+git push origin main
